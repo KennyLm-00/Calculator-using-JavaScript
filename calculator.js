9514dd3e3409927da56180a5c92a104979b2af58
KennyLm-00 <93888732+KennyLm-00@users.noreply.github.com>
2021-11-07 16:07:38 -0800

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,52 @@
+function oneclick() {
+
+    document.getElementById("display").value += "1";   
+}
+function twoclick() {
+    document.getElementById("display").value += "2";   
+}
+function threeclick() {
+    document.getElementById("display").value += "3";   
+}
+function fourclick() {
+    document.getElementById("display").value += "4";   
+}
+function fiveclick() {
+    document.getElementById("display").value += "5";   
+}
+function sixclick() {
+    document.getElementById("display").value += "6";   
+}
+function sevenclick() {
+    document.getElementById("display").value += "7";   
+}
+function eightclick() {
+    document.getElementById("display").value += "8";   
+}
+function nineclick() {
+    document.getElementById("display").value += "9";   
+}
+function tenclick() {
+    document.getElementById("display").value += "10";   
+}
+function zeroclick() {
+    document.getElementById("display").value += "0";   
+}
+function addclick() {
+    document.getElementById("display").value += "+";   
+}
+function subtractclick() {
+    document.getElementById("display").value += "-";   
+}
+function timesclick() {
+    document.getElementById("display").value += "*";   
+}
+function divideclick() {
+    document.getElementById("display").value += "/";   
+}
+function equalclick() {
+    document.getElementById("display").value = eval(document.getElementById("display").value);
+}
+function clearclick() {
+    document.getElementById("display").value = "";
+}
